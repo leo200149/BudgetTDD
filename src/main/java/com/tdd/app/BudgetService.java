ed9bc38b0249d35fda2f8c7e6570a49b34515f0f
leo.chen <leo.chen@aceqilin.com>
2020-09-26 15:25:01 +0800

    update: api method

--- src/main/java/com/tdd/app/BudgetService.java
+++ src/main/java/com/tdd/app/BudgetService.java
@@ -1,4 +1,9 @@
 package com.tdd.app;
 
+import java.util.Date;
+
 public class BudgetService {
+    public double query(Date start, Date end) {
+        return 0;
+    }
 }
---
+++ src/test/java/com/tdd/app/BudgetServiceTest.java
@@ -0,0 +1,7 @@
+package com.tdd.app;
+
+import junit.framework.TestCase;
+
+public class BudgetServiceTest extends TestCase {
+
+}
