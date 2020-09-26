9be92f255e1f4a809a17dabe3d34e75481ee407f
leo.chen <leo.chen@aceqilin.com>
2020-09-26 15:30:09 +0800

    update: repo

--- src/main/java/com/tdd/app/Budget.java
+++ src/main/java/com/tdd/app/Budget.java
@@ -3,4 +3,9 @@ package com.tdd.app;
 public class Budget {
     String yearMonth;
     Integer amount;
+
+    public Budget(String yearMonth, Integer amount) {
+        this.yearMonth = yearMonth;
+        this.amount = amount;
+    }
 }
