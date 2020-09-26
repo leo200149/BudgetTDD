618703401cf5722c4cfddd2f8de8c021f0032d96
Teng <markzog21@gmail.com>
2020-09-26 16:15:19 +0800

    merge

--- src/test/java/com/tdd/app/BudgetServiceTest.java
+++ src/test/java/com/tdd/app/BudgetServiceTest.java
@@ -1,40 +1,47 @@
 package com.tdd.app;
 
 import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+@RunWith(JUnit4.class)
 public class BudgetServiceTest extends TestCase {
 
 
     @Mock
-    private IBudgetRepo repo;
+    private List<Budget> array = new ArrayList<>();
 
-    private BudgetService budgetService;
-
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        repo = new BudgetRepo();
-        budgetService = new BudgetService(repo);
-    }
+    private BudgetService budgetService = new BudgetService(new IBudgetRepo() {
+        @Override
+        public List<Budget> getAll() {
+            return array;
+        }
+    });
 
     @Test
     public void no_budget() {
-
         LocalDate startDate = LocalDate.of(2020, 2, 10);
         LocalDate endDate = LocalDate.of(2020, 3, 10);
         double budget = budgetService.query(startDate, endDate);
-
-
         Assert.assertEquals(0, budget, 0.0);
     }
 
     @Test
     public void test_start_date_later_then_end_date() {
+
+        array.clear();
+
+        array.add(new Budget("202002", 290));
+        array.add(new Budget("202003", 310));
+
         LocalDate endDate = LocalDate.of(2020, 2, 10);
         LocalDate startDate = LocalDate.of(2020, 3, 10);
         double budget = budgetService.query(startDate, endDate);
