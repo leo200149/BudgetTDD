976ede660b0e2c4f1cafb08859019e58cde0338c
Teng <markzog21@gmail.com>
2020-09-26 15:39:32 +0800

    add some test case

--- src/main/java/com/tdd/app/BudgetService.java
+++ src/main/java/com/tdd/app/BudgetService.java
@@ -3,6 +3,13 @@ package com.tdd.app;
 import java.util.Date;
 
 public class BudgetService {
+
+    private IBudgetRepo repo;
+
+    public BudgetService(IBudgetRepo repo){
+        this.repo = repo;
+    }
+
     public double query(Date start, Date end) {
         return 0;
     }
--- src/test/java/com/tdd/app/BudgetServiceTest.java
+++ src/test/java/com/tdd/app/BudgetServiceTest.java
@@ -1,7 +1,24 @@
 package com.tdd.app;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
 
 public class BudgetServiceTest extends TestCase {
+
+    @Test
+    public void single_month_selected() {
+        IBudgetRepo repo = new BudgetRepo();
+
+        BudgetService service = new BudgetService(repo);
+
+        List<Budget> budgetList = service.query(Calendar );
+
+
+        Assert.assertTrue();
+
+    }
 
 }
