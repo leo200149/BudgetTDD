43c28c074372f01f25722a60561af6c6dc5f7cfd
Howard Wang <howard.wang@kyperdata.com>
2020-09-27 09:17:10 +0800

    refactor: cleanup test cases

--- src/test/java/com/tdd/app/BudgetServiceTest.java
+++ src/test/java/com/tdd/app/BudgetServiceTest.java
@@ -1,12 +1,11 @@
 package com.tdd.app;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
-import org.junit.Assert;
-import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -15,19 +14,13 @@ import java.util.List;
 @RunWith(JUnit4.class)
 public class BudgetServiceTest extends TestCase {
 
-
     @Mock
-    private List<Budget> array = new ArrayList<>();
+    private final List<Budget> array = new ArrayList<>();
 
-    private BudgetService budgetService = new BudgetService(new IBudgetRepo() {
-        @Override
-        public List<Budget> getAll() {
-            return array;
-        }
-    });
+    private final BudgetService budgetService = new BudgetService(() -> array);
 
     @Test
-    public void no_budget() {
+    public void test_no_budget() {
         LocalDate startDate = LocalDate.of(2020, 2, 10);
         LocalDate endDate = LocalDate.of(2020, 3, 10);
         double budget = budgetService.query(startDate, endDate);
@@ -36,113 +29,99 @@ public class BudgetServiceTest extends TestCase {
 
     @Test
     public void test_start_date_later_then_end_date() {
-
         array.clear();
-
         array.add(new Budget("202002", 290));
         array.add(new Budget("202003", 310));
 
         LocalDate endDate = LocalDate.of(2020, 2, 10);
         LocalDate startDate = LocalDate.of(2020, 3, 10);
+
         double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(0, budget, 0.0);
     }
 
     @Test
-    public void test_single_day_selected() {
-
+    public void test_query_single_day() {
         array.clear();
-
-        array.add(new Budget("202002", 290));
         array.add(new Budget("202003", 310));
 
         LocalDate startDate = LocalDate.of(2020, 3, 1);
         LocalDate endDate = LocalDate.of(2020, 3, 1);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(10, budget, 0.0);
     }
 
     @Test
-    public void search_test_n_month() {
-
+    public void test_query_multiple_full_month() {
         array.clear();
-
         array.add(new Budget("202002", 2800));
         array.add(new Budget("202003", 310));
 
         LocalDate startDate = LocalDate.of(2020, 2, 1);
         LocalDate endDate = LocalDate.of(2020, 3, 31);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(3110, budget, 0.0);
     }
 
     @Test
-    public void search_test_cross_month_with_no_badget() {
-
+    public void test_query_cross_month_with_no_budget() {
         array.clear();
-
         array.add(new Budget("202002", 0));
         array.add(new Budget("202003", 310));
 
         LocalDate startDate = LocalDate.of(2020, 2, 1);
         LocalDate endDate = LocalDate.of(2020, 3, 31);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(310, budget, 0.0);
     }
 
     @Test
-    public void search_test_cross_month() {
-
+    public void test_query_cross_month() {
         array.clear();
-
         array.add(new Budget("202002", 290));
         array.add(new Budget("202003", 3100));
 
         LocalDate startDate = LocalDate.of(2020, 2, 10);
         LocalDate endDate = LocalDate.of(2020, 3, 10);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(1200, budget, 0.0);
     }
 
     @Test
-    public void search_test_cross_multi_month() {
-
+    public void test_query_cross_multiple_month() {
         array.clear();
-
         array.add(new Budget("202002", 29));
         array.add(new Budget("202003", 310));
         array.add(new Budget("202004", 3000));
 
         LocalDate startDate = LocalDate.of(2020, 2, 10);
         LocalDate endDate = LocalDate.of(2020, 4, 10);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(1330, budget, 0.0);
     }
 
     @Test
-    public void search_test_cross_multi_year() {
-
+    public void test_query_cross_multiple_year() {
         array.clear();
-
         array.add(new Budget("202002", 29));
         array.add(new Budget("202003", 310));
         array.add(new Budget("202004", 3000));
 
         LocalDate startDate = LocalDate.of(2019, 12, 10);
         LocalDate endDate = LocalDate.of(2020, 4, 10);
-        double budget = budgetService.query(startDate, endDate);
 
+        double budget = budgetService.query(startDate, endDate);
 
         Assert.assertEquals(1339, budget, 0.0);
     }
