efea2330ba843cfe702668da26ab060336c92eda
Howard Wang <howard.wang@kyperdata.com>
2020-09-27 16:26:34 +0800

    move calculateDaysOfEachMonth() to Period

--- src/main/java/com/tdd/app/BudgetService.java
+++ src/main/java/com/tdd/app/BudgetService.java
@@ -3,7 +3,6 @@ package com.tdd.app;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -32,48 +31,10 @@ public class BudgetService {
         List<Budget> budgetList = repo.getAll();
 
         // 計算各月份日數
-        Map<String, Integer> targets = calculateDaysOfEachMonth(start, end);
+        Map<String, Integer> targets = new Period(start, end).calculateDaysOfEachMonth();
 
         // 計算預算總和
         return calculateAmount(targets, budgetList);
-    }
-
-    private Map<String, Integer> calculateDaysOfEachMonth(LocalDate startDate, LocalDate endDate) {
-        Map<String, Integer> daysOfEachMonth = new HashMap<>();
-
-        int currentMonthStart = YearMonth.from(startDate).getMonthValue();
-        int currentMonthEnd = YearMonth.from(endDate).getMonthValue();
-        for (int currentYear = YearMonth.from(startDate).getYear(); currentYear <= YearMonth.from(endDate).getYear(); currentYear++) {
-            if (YearMonth.from(startDate).getYear() != YearMonth.from(endDate).getYear()) {
-                if (currentYear == YearMonth.from(startDate).getYear()) {
-                    currentMonthEnd = 12;
-                } else if (currentYear == YearMonth.from(endDate).getYear()) {
-                    currentMonthStart = 1;
-                } else {
-                    currentMonthStart = 1;
-                    currentMonthEnd = 12;
-                }
-            }
-            for (int currentMonth = currentMonthStart; currentMonth <= currentMonthEnd; currentMonth++) {
-                LocalDate overlappedEndDate = YearMonth.of(currentYear, currentMonth).atEndOfMonth();
-                LocalDate overlappedStartDate = YearMonth.of(currentYear, currentMonth).atDay(1);
-                if (currentMonthStart != currentMonthEnd) {
-                    if (currentMonth == currentMonthStart) {
-                        overlappedEndDate = YearMonth.from(startDate).atEndOfMonth();
-                        overlappedStartDate = startDate;
-                    } else if (currentMonth == YearMonth.from(endDate).getMonthValue()) {
-                        overlappedEndDate = endDate;
-                        overlappedStartDate = YearMonth.of(currentYear, currentMonth).atDay(1);
-                    }
-                } else {
-                    overlappedEndDate = endDate;
-                    overlappedStartDate = startDate;
-                }
-                int daysOfCurrentMonth = new Period(overlappedEndDate, overlappedStartDate).calculateDaysOfCurrentMonth();
-                daysOfEachMonth.put(YearMonth.of(currentYear, currentMonth).format(FORMATTER), daysOfCurrentMonth);
-            }
-        }
-        return daysOfEachMonth;
     }
 
     private double calculateAmount(Map<String, Integer> overlappingDaysEachYearMonth, List<Budget> budgetList) {
--- src/main/java/com/tdd/app/Period.java
+++ src/main/java/com/tdd/app/Period.java
@@ -1,12 +1,19 @@
 package com.tdd.app;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Period {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("uuuuMM");
+
     private final LocalDate endDate;
     private final LocalDate startDate;
 
-    public Period(LocalDate endDate, LocalDate startDate) {
+    public Period(LocalDate startDate, LocalDate endDate) {
         this.endDate = endDate;
         this.startDate = startDate;
     }
@@ -21,5 +28,43 @@ public class Period {
 
     int calculateDaysOfCurrentMonth() {
         return getEndDate().getDayOfMonth() - getStartDate().getDayOfMonth() + 1;
+    }
+
+    Map<String, Integer> calculateDaysOfEachMonth() {
+        Map<String, Integer> daysOfEachMonth = new HashMap<>();
+
+        int currentMonthStart = YearMonth.from(getStartDate()).getMonthValue();
+        int currentMonthEnd = YearMonth.from(getEndDate()).getMonthValue();
+        for (int currentYear = YearMonth.from(getStartDate()).getYear(); currentYear <= YearMonth.from(getEndDate()).getYear(); currentYear++) {
+            if (YearMonth.from(getStartDate()).getYear() != YearMonth.from(getEndDate()).getYear()) {
+                if (currentYear == YearMonth.from(getStartDate()).getYear()) {
+                    currentMonthEnd = 12;
+                } else if (currentYear == YearMonth.from(getEndDate()).getYear()) {
+                    currentMonthStart = 1;
+                } else {
+                    currentMonthStart = 1;
+                    currentMonthEnd = 12;
+                }
+            }
+            for (int currentMonth = currentMonthStart; currentMonth <= currentMonthEnd; currentMonth++) {
+                LocalDate overlappedEndDate = YearMonth.of(currentYear, currentMonth).atEndOfMonth();
+                LocalDate overlappedStartDate = YearMonth.of(currentYear, currentMonth).atDay(1);
+                if (currentMonthStart != currentMonthEnd) {
+                    if (currentMonth == currentMonthStart) {
+                        overlappedEndDate = YearMonth.from(getStartDate()).atEndOfMonth();
+                        overlappedStartDate = getStartDate();
+                    } else if (currentMonth == YearMonth.from(getEndDate()).getMonthValue()) {
+                        overlappedEndDate = getEndDate();
+                        overlappedStartDate = YearMonth.of(currentYear, currentMonth).atDay(1);
+                    }
+                } else {
+                    overlappedEndDate = getEndDate();
+                    overlappedStartDate = getStartDate();
+                }
+                int daysOfCurrentMonth = new Period(overlappedStartDate, overlappedEndDate).calculateDaysOfCurrentMonth();
+                daysOfEachMonth.put(YearMonth.of(currentYear, currentMonth).format(FORMATTER), daysOfCurrentMonth);
+            }
+        }
+        return daysOfEachMonth;
     }
 }
